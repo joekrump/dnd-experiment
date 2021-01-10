b23ac4d3c3bd02361031890f3abc88fb23eff779
Joseph Krump <krump.joseph@gmail.com>
2021-01-10 12:29:26 -0800

    Add TypeScript project config

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: ['@snowpack/plugin-typescript'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    installTypes: true,
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};

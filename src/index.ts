b3340f50b1f12387fc4fd54a41afa6afdbec92be
Joseph Krump <krump.joseph@gmail.com>
2021-01-11 14:33:27 -0800

    Start dragging and dropping

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,17 @@ confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
   resize: true,
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
+
+function handleDragStart(element: HTMLElement) {
+  element.style.opacity = '0.2';
+}
+
+function handleDragEnd(element: HTMLElement) {
+  element.style.opacity = '1';
+}
+
+let items = document.querySelectorAll('.container .box') as NodeListOf<HTMLElement>;
+items.forEach(function(item: HTMLElement) {
+  item.addEventListener('dragstart', () => handleDragStart(item), false);
+  item.addEventListener('dragend', () => handleDragEnd(item), false);
+});
